e2c6f74cf78bc63782dea7abbfea3a2753643f3e
Navneet Thukrul <navneetthukrul2000@gmail.com>
2026-09-05 23:30:29 +0530

    Add Google AdSense verification

--- src/app/de/layout.tsx
+++ src/app/de/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="de-DE" suppressHydrationWarning>
       <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2034524057295260" crossOrigin="anonymous"></script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-G2PRBWP394"
           strategy="afterInteractive"
